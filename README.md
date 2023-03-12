354a31466d7a43a3f62144db8a2c19c46ec6a473
Phillip Harden <phillipharden@Phillips-MacBook-Pro.local>
2023-03-12 10:23:35 -0400

    saving in order to organize repo

--- README.md
+++ README.md
@@ -12,17 +12,21 @@ When we install node in our system, it comes with a built-in tool called, node p
 
 Here it is a sample npm command.
 
-> sudo npm install -g express;
+> sudo npm install -g express
 
 ##### Express
 Express is web application framework design for node. You can take a look at the documentation here. [Express](https://expressjs.com/)
+
+To install Express use the terminal command:
+`sudo npm install -g express`
+
 
 This is where you start building MEAN (Mongo,Express, Angular,Node) stack apps. You don't have to use Angular since there are a lots of alternatives available today.
 
 Once you install Express using the framework is pretty easy
 
-var express=require('express'); // tell our application we will use express framework so bring the source
-
+`var express=require('express'); `
+`// tell our application we will use express framework so bring the source`
 `// tell our application that use the framework's features by referring to "app"`
 `var app=express(); `
 `// tell framework that when someone types the root domain name or localhost use this action`
@@ -59,6 +63,48 @@ Now you don't have to keep restarting the app manually. Nodemon keep tracks of t
 ##### Display html page
 Now let's create and html login form and display it in our application. [Follow me here](https://www.youtube.com/watch?v=CEmoFPWXc4c)
 Add this to top of your `app.js"use strict";`
+
+
+___
+
+### EJS
+
+**EJS** (Embedded JavaScript) is a popular templating engine for Node.js applications that allows you to generate dynamic HTML pages using JavaScript code. It is a simple, flexible, and widely used templating engine that is compatible with most web frameworks and is easy to learn.
+
+EJS allows you to embed JavaScript code directly into your HTML templates, making it easy to generate dynamic content and handle complex logic. With EJS, you can easily create reusable components, pass data to templates, and control the layout and structure of your pages.
+
+To install EJS use the terminal command: `sudo npm install -g ejs`
+
+EJS supports a range of features, including:
+
+* Embedding JavaScript code in HTML templates
+* Conditional statements and loops
+* Template inheritance and partials
+* Support for layout files
+* Data injection and processing
+* Support for custom tags and filters
+
+EJS is widely used in the Node.js community and is compatible with most web frameworks, including Express, Koa, and Hapi. It is a popular choice for building dynamic web applications and generating HTML pages on the server-side.
+
+
+### Body Parser
+
+To install, use the terminal command: `sudo npm install -g body-parser`
+
+**body-parser** is a middleware module for Express, a popular web framework for Node.js. It helps to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+
+In simpler terms, when a user submits data via a form or sends data through an HTTP request, the **body-parser** middleware helps to extract the data from the request and make it available in a format that's easier for you to work with in your code.
+
+**body-parser** supports parsing of various request data types including JSON and url-encoded data. The package provides several middleware functions that can be used based on the type of data being submitted. Some of the middleware functions provided by body-parser include json(), urlencoded(), and raw().
+
+**body-parser** is widely used in the Node.js and Express community and is considered a standard middleware for handling form data in Express applications. It makes it easier to work with incoming request data, reduces boilerplate code, and simplifies data validation and error handling.
+
+
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,66 +1,40 @@
 "use strict";
-//^ tell our application we will use express framework so bring the source
-//? This line imports the express module and assigns it to the variable express. This line is required at the beginning of any Node.js file that uses the Express framework.
-var express=require('express'); 
-//^ tell our application that use the framework's features by referring to "app"
-//? This line creates an instance of the Express application and assigns it to the variable app. The app variable will be used to define routes and start the server.
-var app=express(); 
-//^ tell framework that when someone types the root domain name or localhost use this action
-//? This line sets up a route handler for GET requests to the root URL (/). When a client makes a GET request to the root URL, the function specified in the second argument of app.get() will be called. In this case, the function sends the string 'Hello SSL Students!' as the response to the client.
-app.get('/',function(req,res) 
-{
-//^ show users a message when the get("/") is called
-res.send('Hello SSL Students!'); 
-});
-//^ start the server by using the previous instructions.
-//? This line starts the server and tells it to listen for incoming requests on port 3000. The second argument is a callback function that will be called once the server has started listening for requests. In this example, the function is empty, so it doesn't do anything. However, you could use this callback function to log a message or perform other setup tasks. The server variable is used to keep a reference to the server instance, which can be useful for stopping the server later on.
-var server=app.listen(3000,function() {}); 
-
-
-
-
-
-
 
 let fs = require("fs");
 let http = require("http");
 let path = require("path");
 let url = require("url");
-let hostname = "localhost";
-let port = 8080;
-http.createServer(function(req, res){
-    let parsed = url.parse(req.url);
-    let filename = path.parse(parsed.pathname);
-    let file_name = filename.name==""?"index":filename.name;
-    let ext = filename.ext==""?".html":filename.ext;
-    let dir = filename.dir=="/"?"":filename.dir+"/";
-    let page = filename.name==""?"index.html":filename.name;
-    f = (dir+file_name+ext).replace("/","");
-    let mimeTypes = {
-        '.html':'text/html',
-        '.js':'text/javascript',
-        '.css':'text/css',
-        '.png':'image/png',
-        '.jpg':'image/jpg',
-        '.gif':'image/gif',
-    };    
-    if (f) {
-        fs.readFile(f,function(err,data){
-            if(page){
-                if(mimeTypes.hasOwnProperty(ext)){
-                    res.writeHead(200, {'Content-Type': 'mimeTypes.'+ext});
-                    res.write("<script>let page=`" + file_name + "`;</script>");
-                    res.end(data, 'utf-8');
-                }
-            }
-        })
-    }
-  }).listen(port, function(){
-    console.log("info:",`Server is running at http://${hostname}:${port}/`);
-  });
 
+let express = require("express");
+let request = require("request");
+let bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  // COLORFUL COMMENTS
+// sudo npm install -g express
+// sudo npm install -g ejs
+// sudo npm install -g request
+// sudo npm install -g body-parser
+
+let ejs = require("ejs");
+const router = express.Router();
+let app = express();
+app.set("view engine", "ejs");
+app.engine("ejs", require("ejs")._express);
+
+router.get("/", function (req, res) {
+  res.render("index", { pagename: "Home" }); //views/index.ejs
+});
+
+router.get("/about", function (req, res) {
+  res.render("index", { pagename: "About" }); //views/about.ejs
+});
+
+app.use(express.static("public"));
+app._router("/", router);
+let server = app.listen("8080");
+
+// COLORFUL COMMENTS
 //! Red (!)
 //? Blue (?)
 //* Green (*)
